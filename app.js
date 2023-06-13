267264f3c47dae3ba04f54c3da8f0f8d9dae92d5
Hayk-Kharatyan <kharatyan.hayk007@gmail.com>
2023-06-13 16:05:45 +0400

    added mini image click on carusel

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let arrImg = ["img1.jpg", "img2.jpg", "img3.jpg"]
 let i = 0
+
 document.querySelector(".left").addEventListener("click", function () {
     if (i > 0) {
 
@@ -17,15 +18,71 @@ document.querySelector(".left").addEventListener("click", function () {
 
 
 
-document.querySelector(".right").addEventListener("click", function () {
-    if (i < arrImg.length - 1 ) {
+function rightclick(){
+   
 
-        i++
+    if(i<arrImg.length - 1 ){
+    
+    i++
 
+    
     }
-    else {
+    
+    
+    else{
+       
         i = 0
-    }
 
-    document.querySelector(".img1").src = arrImg[i]
-})
+    }
+    
+   document.querySelector(".img1").src = arrImg[i]  
+   document.querySelector(".p").innerHTML= i + 1
+}
+
+let rightbtn = document.querySelector(".right")
+
+rightbtn.addEventListener("click",function(){
+    rightclick()
+ })
+ 
+ setInterval(rightclick, 3000);
+
+let mini_images=document.querySelectorAll(".images")
+let bigimg = document.querySelector(".img1")
+let toggle1 = document.querySelector("#toggle1")
+let toggle2 = document.querySelector("#toggle2")
+let toggle3 = document.querySelector("#toggle3")
+for(let j = 0; j<mini_images.length;j++){
+
+    mini_images[j].addEventListener("click",function(){
+        let src = this.src
+        let id = this.id
+        bigimg.src=src
+
+        if(id==="toggle1"){
+            toggle1.classList.add("checkedimage")
+            toggle2.classList.remove("checkedimage")
+            toggle3.classList.remove("checkedimage")
+        }
+
+        
+        if(id==="toggle2"){
+            toggle2.classList.add("checkedimage")
+            toggle1.classList.remove("checkedimage")
+            toggle3.classList.remove("checkedimage")
+        }
+
+        
+        if(id==="toggle3"){
+            toggle3.classList.add("checkedimage")
+            toggle1.classList.remove("checkedimage")
+            toggle2.classList.remove("checkedimage")
+        }
+    })
+
+}
+
+
+
+
+
